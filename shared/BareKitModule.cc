0313debb3d865c921ddf77b9f5f80aa25875d811
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-09-12 12:45:25 +0200

    Ensure worklets are terminated when `HostObject` destructor runs

--- shared/BareKitModule.cc
+++ shared/BareKitModule.cc
@@ -18,7 +18,39 @@ typedef struct uv_buf_t {
   char *base;
   size_t len;
 } uv_buf_t;
+}
 
+namespace facebook::react {
+
+namespace {
+
+static inline uv_buf_t
+jsi_to_buffer(Runtime &rt, const ArrayBuffer &value) {
+  return {reinterpret_cast<char *>(value.data(rt)), value.size(rt)};
+}
+
+static inline uv_buf_t
+jsi_to_buffer(Runtime &rt, const ArrayBuffer &value, size_t offset, size_t length) {
+  return {reinterpret_cast<char *>(value.data(rt)) + offset, length};
+}
+
+static inline uv_buf_t
+jsi_to_buffer(Runtime &rt, const String &value) {
+  auto utf8 = value.utf8(rt);
+
+  return {strdup(utf8.c_str()), utf8.size()};
+}
+
+static inline char *
+jsi_to_string(Runtime &rt, const String &value) {
+  return strdup(value.utf8(rt).c_str());
+}
+
+} // namespace
+
+} // namespace facebook::react
+
+extern "C" {
 typedef struct bare_worklet_s bare_worklet_t;
 typedef struct bare_worklet_options_s bare_worklet_options_t;
 typedef struct bare_ipc_s bare_ipc_t;
@@ -109,17 +141,9 @@ int
 bare_ipc_poll_stop(bare_ipc_poll_t *poll);
 }
 
+namespace facebook::react {
+
 namespace {
-
-struct BareKitWorklet : HostObject {
-  bare_worklet_t *worklet;
-  bare_ipc_t *ipc;
-  bare_ipc_poll_t *poll;
-
-  AsyncCallback<bool, bool> on_poll;
-
-  BareKitWorklet(Runtime &rt, Function &&on_poll, std::shared_ptr<CallInvoker> jsInvoker) : on_poll(rt, std::move(on_poll), jsInvoker) {}
-};
 
 struct BareKitBuffer : MutableBuffer {
   BareKitBuffer(size_t len) : data_(len) {}
@@ -138,295 +162,361 @@ private:
   std::vector<uint8_t> data_;
 };
 
+struct BareKitWorklet : HostObject {
+  bare_worklet_t *worklet = nullptr;
+  bare_ipc_t *ipc = nullptr;
+  bare_ipc_poll_t *poll = nullptr;
+
+  AsyncCallback<bool, bool> on_poll;
+
+  bool started = false;
+  bool terminated = false;
+
+  BareKitWorklet(Runtime &rt, int memoryLimit, std::optional<String> assets, Function &&on_poll, std::shared_ptr<CallInvoker> jsInvoker) : on_poll(rt, std::move(on_poll), jsInvoker) {
+    int err;
+
+    err = bare_worklet_alloc(&worklet);
+    assert(err == 0);
+
+    err = bare_ipc_alloc(&ipc);
+    assert(err == 0);
+
+    err = bare_ipc_poll_alloc(&poll);
+    assert(err == 0);
+
+    bare_ipc_poll_set_data(poll, this);
+
+    bare_worklet_options_t options;
+
+    options.memory_limit = static_cast<size_t>(memoryLimit);
+
+    if (assets) {
+      options.assets = jsi_to_string(rt, assets.value());
+    } else {
+      options.assets = nullptr;
+    }
+
+    err = bare_worklet_init(worklet, &options);
+    assert(err == 0);
+
+    free(const_cast<char *>(options.assets));
+  }
+
+  ~BareKitWorklet() {
+    terminate();
+  }
+
+  void
+  start(Runtime &rt, Object handle, String filename, Array args) {
+    int err;
+
+    if (started || terminated) return;
+
+    started = true;
+
+    auto string = jsi_to_string(rt, filename);
+
+    auto argv = std::vector<const char *>(args.size(rt));
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
+    }
+
+    err = bare_worklet_start(worklet, string, nullptr, nullptr, nullptr, argv.size(), argv.data());
+    assert(err == 0);
+
+    free(string);
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      free(const_cast<char *>(argv[i]));
+    }
+
+    err = bare_ipc_init(ipc, worklet);
+    assert(err == 0);
+
+    err = bare_ipc_poll_init(poll, ipc);
+    assert(err == 0);
+  }
+
+  void
+  start(Runtime &rt, Object handle, String filename, Object source, size_t offset, size_t length, Array args) {
+    int err;
+
+    if (started || terminated) return;
+
+    started = true;
+
+    auto string = jsi_to_string(rt, filename);
+
+    auto buffer = jsi_to_buffer(rt, source.getArrayBuffer(rt), offset, length);
+
+    auto argv = std::vector<const char *>(args.size(rt));
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
+    }
+
+    err = bare_worklet_start(worklet, string, &buffer, nullptr, nullptr, argv.size(), argv.data());
+    assert(err == 0);
+
+    free(string);
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      free(const_cast<char *>(argv[i]));
+    }
+
+    err = bare_ipc_init(ipc, worklet);
+    assert(err == 0);
+
+    err = bare_ipc_poll_init(poll, ipc);
+    assert(err == 0);
+  }
+
+  void
+  start(Runtime &rt, Object handle, String filename, String source, Array args) {
+    int err;
+
+    if (started || terminated) return;
+
+    started = true;
+
+    auto string = jsi_to_string(rt, filename);
+
+    auto buffer = jsi_to_buffer(rt, source);
+
+    auto argv = std::vector<const char *>(args.size(rt));
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
+    }
+
+    err = bare_worklet_start(worklet, string, &buffer, on_free_, nullptr, argv.size(), argv.data());
+    assert(err == 0);
+
+    free(string);
+
+    for (size_t i = 0, n = argv.size(); i < n; i++) {
+      free(const_cast<char *>(argv[i]));
+    }
+
+    err = bare_ipc_init(ipc, worklet);
+    assert(err == 0);
+
+    err = bare_ipc_poll_init(poll, ipc);
+    assert(err == 0);
+  }
+
+  std::optional<Object>
+  read(Runtime &rt, Object handle) {
+    int err;
+
+    if (!started || terminated) return std::nullopt;
+
+    uint8_t *data;
+    size_t len;
+    err = bare_ipc_read(ipc, reinterpret_cast<void **>(&data), &len);
+    assert(err == 0 || err == bare_ipc_would_block);
+
+    if (err == bare_ipc_would_block) {
+      return std::nullopt;
+    }
+
+    auto buffer = std::make_shared<BareKitBuffer>(len);
+
+    std::copy(data, data + len, buffer->data());
+
+    return ArrayBuffer(rt, buffer);
+  }
+
+  int
+  write(Runtime &rt, Object handle, Object data, size_t offset, size_t length) {
+    int err;
+
+    auto buffer = jsi_to_buffer(rt, data.getArrayBuffer(rt), offset, length);
+
+    err = bare_ipc_write(ipc, buffer.base, buffer.len);
+    assert(err >= 0 || err == bare_ipc_would_block);
+
+    if (err == bare_ipc_would_block) {
+      return 0;
+    }
+
+    return err;
+  }
+
+  void
+  update(bool readable, bool writable) {
+    int err;
+
+    if (terminated) return;
+
+    int events = 0;
+
+    if (readable) events |= bare_ipc_readable;
+    if (writable) events |= bare_ipc_writable;
+
+    if (events) {
+      err = bare_ipc_poll_start(poll, events, on_poll_);
+      assert(err == 0);
+    } else {
+      err = bare_ipc_poll_stop(poll);
+      assert(err == 0);
+    }
+  }
+
+  void
+  suspend(int linger) {
+    int err;
+
+    if (!started || terminated) return;
+
+    err = bare_worklet_suspend(worklet, linger);
+    assert(err == 0);
+  }
+
+  void
+  resume() {
+    int err;
+
+    if (!started || terminated) return;
+
+    err = bare_worklet_resume(worklet);
+    assert(err == 0);
+  }
+
+  void
+  wakeup(int deadline) {
+    int err;
+
+    if (!started || terminated) return;
+
+    err = bare_worklet_wakeup(worklet, int(deadline));
+    assert(err == 0);
+  }
+
+  void
+  terminate() {
+    int err;
+
+    if (terminated) return;
+
+    terminated = true;
+
+    if (started) {
+      err = bare_worklet_terminate(worklet);
+      assert(err == 0);
+
+      bare_ipc_poll_destroy(poll);
+      bare_ipc_destroy(ipc);
+
+      free(ipc);
+      free(poll);
+    }
+
+    bare_worklet_destroy(worklet);
+
+    free(worklet);
+
+    worklet = nullptr;
+    ipc = nullptr;
+    poll = nullptr;
+  }
+
+private:
+  static void
+  on_poll_(bare_ipc_poll_t *poll, int events) {
+    auto worklet = static_cast<BareKitWorklet *>(bare_ipc_poll_get_data(poll));
+
+    worklet->on_poll.call([=](Runtime &rt, Function &function) {
+      function.call(rt, (events & bare_ipc_readable) != 0, (events & bare_ipc_writable) != 0);
+    });
+  }
+
+  static void
+  on_free_(bare_worklet_t *, const uv_buf_t *source, void *) {
+    free(source->base);
+  }
+};
+
 } // namespace
-
-namespace {
-
-static inline uv_buf_t
-jsi_to_buffer(Runtime &rt, const ArrayBuffer &value) {
-  return {reinterpret_cast<char *>(value.data(rt)), value.size(rt)};
-}
-
-static inline uv_buf_t
-jsi_to_buffer(Runtime &rt, const ArrayBuffer &value, size_t offset, size_t length) {
-  return {reinterpret_cast<char *>(value.data(rt)) + offset, length};
-}
-
-static inline uv_buf_t
-jsi_to_buffer(Runtime &rt, const String &value) {
-  auto utf8 = value.utf8(rt);
-
-  return {strdup(utf8.c_str()), utf8.size()};
-}
-
-static inline char *
-jsi_to_string(Runtime &rt, const String &value) {
-  return strdup(value.utf8(rt).c_str());
-}
-
-} // namespace
-
-namespace facebook::react {
 
 BareKitModule::BareKitModule(std::shared_ptr<CallInvoker> jsInvoker) : NativeBareKitCxxSpec(std::move(jsInvoker)) {}
 
 Object
 BareKitModule::init(Runtime &rt, double memoryLimit, std::optional<String> assets, Function poll) {
-  int err;
-
-  auto worklet = std::make_shared<BareKitWorklet>(rt, std::move(poll), jsInvoker_);
-
-  err = bare_worklet_alloc(&worklet->worklet);
-  assert(err == 0);
-
-  err = bare_ipc_alloc(&worklet->ipc);
-  assert(err == 0);
-
-  err = bare_ipc_poll_alloc(&worklet->poll);
-  assert(err == 0);
-
-  bare_ipc_poll_set_data(worklet->poll, worklet.get());
-
-  bare_worklet_options_t options;
-
-  options.memory_limit = static_cast<size_t>(memoryLimit);
-
-  if (assets) {
-    options.assets = jsi_to_string(rt, assets.value());
-  } else {
-    options.assets = nullptr;
-  }
-
-  err = bare_worklet_init(worklet->worklet, &options);
-  assert(err == 0);
-
-  free(const_cast<char *>(options.assets));
+  auto worklet = std::make_shared<BareKitWorklet>(rt, int(memoryLimit), std::move(assets), std::move(poll), jsInvoker_);
 
   return Object::createFromHostObject(rt, std::move(worklet));
 }
 
-namespace {
-
-static void
-bare_kit_module__on_poll(bare_ipc_poll_t *poll, int events) {
-  auto worklet = static_cast<BareKitWorklet *>(bare_ipc_poll_get_data(poll));
-
-  worklet->on_poll.call([=](jsi::Runtime &rt, jsi::Function &function) {
-    function.call(rt, (events & bare_ipc_readable) != 0, (events & bare_ipc_writable) != 0);
-  });
-}
-
-} // namespace
-
 void
 BareKitModule::update(Runtime &rt, Object handle, bool readable, bool writable) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  int events = 0;
-
-  if (readable) events |= bare_ipc_readable;
-  if (writable) events |= bare_ipc_writable;
-
-  if (events) {
-    err = bare_ipc_poll_start(worklet->poll, events, bare_kit_module__on_poll);
-    assert(err == 0);
-  } else {
-    err = bare_ipc_poll_stop(worklet->poll);
-    assert(err == 0);
-  }
+  worklet->update(readable, writable);
 }
 
 void
 BareKitModule::startFile(Runtime &rt, Object handle, String filename, Array args) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  auto string = jsi_to_string(rt, filename);
-
-  auto argv = std::vector<const char *>(args.size(rt));
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
-  }
-
-  err = bare_worklet_start(worklet->worklet, string, nullptr, nullptr, nullptr, argv.size(), argv.data());
-  assert(err == 0);
-
-  free(string);
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    free(const_cast<char *>(argv[i]));
-  }
-
-  err = bare_ipc_init(worklet->ipc, worklet->worklet);
-  assert(err == 0);
-
-  err = bare_ipc_poll_init(worklet->poll, worklet->ipc);
-  assert(err == 0);
+  worklet->start(rt, std::move(handle), std::move(filename), std::move(args));
 }
 
 void
 BareKitModule::startBytes(Runtime &rt, Object handle, String filename, Object source, double offset, double length, Array args) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  auto string = jsi_to_string(rt, filename);
-
-  auto buffer = jsi_to_buffer(rt, source.getArrayBuffer(rt), size_t(offset), size_t(length));
-
-  auto argv = std::vector<const char *>(args.size(rt));
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
-  }
-
-  err = bare_worklet_start(worklet->worklet, string, &buffer, nullptr, nullptr, argv.size(), argv.data());
-  assert(err == 0);
-
-  free(string);
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    free(const_cast<char *>(argv[i]));
-  }
-
-  err = bare_ipc_init(worklet->ipc, worklet->worklet);
-  assert(err == 0);
-
-  err = bare_ipc_poll_init(worklet->poll, worklet->ipc);
-  assert(err == 0);
+  worklet->start(rt, std::move(handle), std::move(filename), std::move(source), size_t(offset), size_t(length), std::move(args));
 }
-
-namespace {
-
-static void
-bare_kit_module__on_finalize(bare_worklet_t *, const uv_buf_t *source, void *) {
-  free(source->base);
-}
-
-} // namespace
 
 void
 BareKitModule::startUTF8(Runtime &rt, Object handle, String filename, String source, Array args) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  auto string = jsi_to_string(rt, filename);
-
-  auto buffer = jsi_to_buffer(rt, source);
-
-  auto argv = std::vector<const char *>(args.size(rt));
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    argv[i] = jsi_to_string(rt, args.getValueAtIndex(rt, i).getString(rt));
-  }
-
-  err = bare_worklet_start(worklet->worklet, string, &buffer, bare_kit_module__on_finalize, nullptr, argv.size(), argv.data());
-  assert(err == 0);
-
-  free(string);
-
-  for (size_t i = 0, n = argv.size(); i < n; i++) {
-    free(const_cast<char *>(argv[i]));
-  }
-
-  err = bare_ipc_init(worklet->ipc, worklet->worklet);
-  assert(err == 0);
-
-  err = bare_ipc_poll_init(worklet->poll, worklet->ipc);
-  assert(err == 0);
+  worklet->start(rt, std::move(handle), std::move(filename), std::move(source), std::move(args));
 }
 
 std::optional<Object>
 BareKitModule::read(Runtime &rt, Object handle) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  uint8_t *data;
-  size_t len;
-  err = bare_ipc_read(worklet->ipc, reinterpret_cast<void **>(&data), &len);
-  assert(err == 0 || err == bare_ipc_would_block);
-
-  if (err == bare_ipc_would_block) {
-    return std::nullopt;
-  }
-
-  auto buffer = std::make_shared<BareKitBuffer>(len);
-
-  std::copy(data, data + len, buffer->data());
-
-  return ArrayBuffer(rt, buffer);
+  return worklet->read(rt, std::move(handle));
 }
 
 double
 BareKitModule::write(Runtime &rt, Object handle, Object data, double offset, double length) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  auto buffer = jsi_to_buffer(rt, data.getArrayBuffer(rt), size_t(offset), size_t(length));
-
-  err = bare_ipc_write(worklet->ipc, buffer.base, buffer.len);
-  assert(err >= 0 || err == bare_ipc_would_block);
-
-  if (err == bare_ipc_would_block) {
-    return 0;
-  }
-
-  return double(err);
+  return double(worklet->write(rt, std::move(handle), std::move(data), size_t(offset), size_t(length)));
 }
 
 void
 BareKitModule::suspend(Runtime &rt, Object handle, double linger) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  err = bare_worklet_suspend(worklet->worklet, int(linger));
-  assert(err == 0);
+  worklet->suspend(int(linger));
 }
 
 void
 BareKitModule::resume(Runtime &rt, Object handle) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  err = bare_worklet_resume(worklet->worklet);
-  assert(err == 0);
+  worklet->resume();
 }
 
 void
 BareKitModule::wakeup(Runtime &rt, Object handle, double deadline) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  err = bare_worklet_wakeup(worklet->worklet, int(deadline));
-  assert(err == 0);
+  worklet->wakeup(int(deadline));
 }
 
 void
 BareKitModule::terminate(Runtime &rt, Object handle) {
-  int err;
-
   auto worklet = handle.getHostObject<BareKitWorklet>(rt);
 
-  err = bare_worklet_terminate(worklet->worklet);
-  assert(err == 0);
-
-  bare_ipc_poll_destroy(worklet->poll);
-  bare_ipc_destroy(worklet->ipc);
-  bare_worklet_destroy(worklet->worklet);
-
-  free(worklet->worklet);
-  free(worklet->ipc);
-  free(worklet->poll);
+  worklet->terminate();
 }
 
 } // namespace facebook::react
