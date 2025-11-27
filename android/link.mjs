5d40707cf5838665a1db99822d112a3f949feeb5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-11-27 13:59:58 +0100

    Remove broken `needs` option

--- android/link.mjs
+++ android/link.mjs
@@ -6,7 +6,6 @@ const __filename = fileURLToPath(import.meta.url)
 
 for await (const resource of link(path.join(__filename, '..', '..', '..', '..'), {
   target: ['android-arm64', 'android-arm', 'android-ia32', 'android-x64'],
-  needs: ['libbare-kit.so'],
   out: path.join(__filename, '..', 'src', 'main', 'addons')
 })) {
   console.log('Wrote', resource)
