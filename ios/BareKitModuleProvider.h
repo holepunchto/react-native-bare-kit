89b270e76c726e7ab38e9b91c31bcc08a37079e4
Dario Moceri <dario.moceri@bitfinex.com>
2025-12-16 11:50:56 +0100

    Older React Native versions (< 0.76): imports RCTTurboModule.h as before. React Native 0.76+: uses a forward declaration since the header was removed.

--- ios/BareKitModuleProvider.h
+++ ios/BareKitModuleProvider.h
@@ -1,5 +1,10 @@
 #import <Foundation/Foundation.h>
+
+#if __has_include(<ReactCommon/RCTTurboModule.h>)
 #import <ReactCommon/RCTTurboModule.h>
+#else
+@protocol RCTModuleProvider;
+#endif
 
 @interface BareKitModuleProvider : NSObject <RCTModuleProvider>
 
