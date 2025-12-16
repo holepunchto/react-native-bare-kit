cc2e13d4750e18417cd11be39ec64ca3af967ce1
Dario Moceri <dario.moceri@bitfinex.com>
2025-12-16 12:04:43 +0100

    remove RCTModuleProvider

--- ios/BareKitModuleProvider.h
+++ ios/BareKitModuleProvider.h
@@ -1,12 +1,5 @@
 #import <Foundation/Foundation.h>
 
-#if __has_include(<ReactCommon/RCTTurboModule.h>)
-#import <ReactCommon/RCTTurboModule.h>
-#endif
-
 @interface BareKitModuleProvider : NSObject
-#if __has_include(<ReactCommon/RCTTurboModule.h>)
-<RCTModuleProvider>
-#endif
 
 @end
